aad5fe521b37be8a00db4bc261725cfa0f603474
trival <thomas.gorny@mail.com>
2017-04-03 10:21:02 +0200

    update libs and add tvs tools

--- src/tiles/flow.ts
+++ src/tiles/flow.ts
@@ -1,6 +1,10 @@
-import * as runtime from 'tvs-flow/lib/runtime'
+import * as runtime from 'tvs-flow/dist/lib/runtime'
+import * as tvsTools from 'tvs-flow-tools'
 export * from 'tvs-flow/lib/utils/entity-reference'
 
 export const flow = runtime.create()
 
+export const tools = tvsTools.ui.start('tiles')
+
 window['flow'] = flow
+window['tools'] = tools
--- src/tiles/index.ts
+++ src/tiles/index.ts
@@ -1,4 +1,4 @@
-import { flow } from './flow'
+import { flow, tools } from './flow'
 import { updateFlow } from "shared-utils/reload";
 
 
@@ -7,6 +7,7 @@ const graphModules = require.context('./graph', true, /\.ts$/)
 
 flow.setDebug(true)
 updateFlow(flow, graphModules)
+tools.updateFlow(flow)
 setTimeout(function () {
   flow.setDebug(false)
 }, 1000)
@@ -16,5 +17,6 @@ if (module.hot) {
   module.hot.accept((graphModules as any).id, function() {
     const newGraphModules = require.context('./graph', true, /\.ts$/)
     updateFlow(flow, newGraphModules)
+    tools.updateFlow(flow)
   })
 }
