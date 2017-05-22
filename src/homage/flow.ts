158dbc2004b7f82b6dea716bc6bc4423c7c88c01
trival <thomas.gorny@mail.com>
2017-05-22 13:33:14 +0200

    save node position of homage graph

--- src/homage/flow.ts
+++ src/homage/flow.ts
@@ -1,10 +1,11 @@
 import * as runtime from 'tvs-flow/dist/lib/runtime'
 import * as tvsTools from 'tvs-flow-tools'
 export * from 'tvs-flow/lib/utils/entity-reference'
+import graph from './nodes.json'
 
 export const flow = runtime.create()
 
-export const tools = tvsTools.ui.start('hommage')
+export const tools = tvsTools.ui.start('hommage', {graph})
 
 window['flow'] = flow
 window['tools'] = tools
