7b06f1a3f61510293c67720f8c2992bbfe647ed2
trival <thomas.gorny@mail.com>
2017-05-22 13:32:57 +0200

    update reload flow graph logic

--- src/shared-utils/reload.ts
+++ src/shared-utils/reload.ts
@@ -1,22 +1,9 @@
 import * as flowTree from 'tvs-libs/lib/flow/tree'
-import { resolveEntityIds, isEntity, getGraphFromAll } from "tvs-flow/lib/utils/entity-reference";
-
-
-export function modulePathToNamespace(path) {
-  return path.split('.')[1].split('/').filter(v => v).join('.')
-}
+import { getGraphFromModules } from "tvs-flow-tools/dist/lib/utils/webpack"
 
 
 export function updateFlow(flow, graphModules) {
-  const entities = graphModules.keys()
-    .map(path => {
-      const module = graphModules(path)
-      return Object.values(resolveEntityIds(module, modulePathToNamespace(path)))
-        .filter(isEntity)
-    })
-    .reduce((arr, es) => arr.concat(es), [])
-
-  flow.addGraph(getGraphFromAll(entities))
+  flow.addGraph(getGraphFromModules(graphModules))
   window['entities'] = flowTree.create(flow)
 }
 
