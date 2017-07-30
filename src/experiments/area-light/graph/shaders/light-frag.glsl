b968f0fbd66716549acdf5405b6eb5f299aaf9e8
trival <thomas.gorny@mail.com>
2017-07-31 00:25:17 +0200

    first working area light implementation

--- src/experiments/area-light/graph/shaders/light-frag.glsl
+++ src/experiments/area-light/graph/shaders/light-frag.glsl
@@ -12,29 +12,30 @@ uniform mat4 view;
 varying vec2 coords;
 
 
-float areaLampEnergy(mat4 lampMat, vec3 V, vec3 N, vec3 lampPos, vec2 areaSize) {
+float areaLampEnergy(mat4 lampMat, vec3 V, vec3 N, vec2 areaSize) {
 	float width = areaSize.x / 2.0;
 	float height = areaSize.y / 2.0;
 
-	vec3 right = normalize(vec3(lampMat * vec4(1.0, 0.0, 0.0, 0.0))); //lamp right axis
-	vec3 up = normalize(vec3(lampMat * vec4(0.0, 1.0, 0.0, 0.0))); //lamp up axis
-	vec3 lampv = normalize(vec3(lampMat * vec4(0.0, 0.0, -1.0, 0.0))); //lamp projection axis
+	vec3 right = normalize(lightMat[0].xyz);
+	vec3 up = normalize(lightMat[1].xyz);
+	vec3 normal = normalize(-1.0 * lightMat[2].xyz);
+	vec3 pos = lightMat[3].xyz;
 
 	/*project onto plane and calculate direction from center to the projection*/
-	float dis = dot(lampv, V - lampPos);
-	vec3 projection = V - dis * lampv;
-	vec3 dir = projection - lampPos;
+	float dis = dot(normal, V - pos);
+	vec3 projection = V - dis * normal;
+	vec3 dir = projection - pos;
 
 	/*calculate distance from area*/
 	vec2 diagonal = vec2(dot(dir, right), dot(dir, up));
 	vec2 nearest2D = vec2(clamp(diagonal.x, -width, width), clamp(diagonal.y, -height, height));
-	vec3 nearestPointInside = lampPos + (right * nearest2D.x + up * nearest2D.y);
+	vec3 nearestPointInside = pos + (right * nearest2D.x + up * nearest2D.y);
 
 	float d = distance(V, nearestPointInside); //real distance to area rectangle
 
 	vec3 L = normalize(nearestPointInside - V);
 
-	float nDotL = clamp(dot(-lampv, L), 0.0, 1.0);
+	float nDotL = clamp(dot(-normal, L), 0.0, 1.0);
 	//nDotL *= clamp(dot(-N,L),0.0,1.0); //how blender internal works, but too hard falloff
 	nDotL *= clamp(dot(-N, L) * 0.5 + 0.5, 0.0, 1.0); //distribute over hemisphere, looks better. Should be dependant of area size.
 
@@ -43,34 +44,25 @@ float areaLampEnergy(mat4 lampMat, vec3 V, vec3 N, vec3 lampPos, vec2 areaSize) 
 	return max(nDotL, 0.0) * attenuation;
 }
 
-void areaDiff(vec3 V, vec3 lampPos, vec3 lampVec, vec3 N, mat4 lampMat, vec2 areaSize, float dist, float k, out float inp) {
-	vec3 vec = V - lampPos;
 
-	float strength = dist * dist / 4.0;
-
-	if (dot(vec, lampVec) < 0.0) {
-		inp = 0.0;
-	} else {
-		float intens = areaLampEnergy(lampMat, V, N, lampPos, areaSize);
-		inp = pow(intens * strength, k);
-	}
-}
+const float lightDistance = 3.0;
 
 void main() {
 
 	vec4 position = texture2D(positions, coords);
 	vec4 normal = texture2D(normals, coords);
-	vec4 uv = texture2D(uvs, coords);
+	//vec4 uv = texture2D(uvs, coords);
 	vec4 color = texture2D(colors, coords);
-
-	vec3 lightPosition = lightMat[3].xyz;
-	vec3 lightNormal = normalize(lightMat[2].xyz);
-	float dist = abs(length(eyePosition - position.xyz));
 
 	float diff = 1.0;
 
-	areaDiff(position.xyz, lightPosition, -lightNormal, normalize(normal.xyz), lightMat, vec2(10.0), dist, 1.0, diff);
-	//gl_FragColor = vec4(abs(normal.xyz), 1.0);
-	gl_FragColor = vec4(color.xyz * diff, 1.0);
-	//gl_FragColor = vec4(color.xyz * (lightPosition / 5.0), 1.0);
+	if (color.a < 1.0) {
+		gl_FragColor = vec4(color.rgb, 1.0);
+	} else {
+		diff = pow(areaLampEnergy(lightMat, position.xyz, normalize(normal.xyz), vec2(10.0)), 0.7);
+		//gl_FragColor = vec4(abs(normal.xyz), 1.0);
+		gl_FragColor = vec4(color.xyz * min(1.2, max(0.2, diff * lightDistance)), 1.0);
+		//gl_FragColor = vec4(color.xyz * (lightPosition / 5.0), 1.0);
+	}
+
 }
--- src/experiments/area-light/graph/state.ts
+++ src/experiments/area-light/graph/state.ts
@@ -1,5 +1,6 @@
 import { stream, streamStart, val } from 'tvs-flow/dist/lib/utils/entity-reference'
 import { mat4 } from 'gl-matrix'
+import { tick } from './events'
 
 
 export const groundColor = val([0.5, 0.5, 0.5, 1.0])
@@ -11,10 +12,12 @@ export const groundTransform = streamStart(null, () => {
 	return t
 })
 
-export const lightRotation = val(Math.PI / 6)
-export const lightPosition = val([0, 5, 0])
+export const lightRotation = val(-Math.PI * 0.8)
+.react([tick.HOT], (rot, _) => rot + 0.02)
 
-export const lightColor = val([1.0, 1.0, 1.0, 1.0])
+export const lightPosition = val([0, 5.5, 0])
+
+export const lightColor = val([1.0, 1.0, 1.0, 0.0])
 
 export const lightTransform = stream(
 	[lightRotation.HOT, lightPosition.HOT],
