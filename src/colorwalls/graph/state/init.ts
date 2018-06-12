dc30dcc8fb3619de72ec908de32ce6eda7f85724
trival <thomas.gorny@mail.com>
2018-06-12 18:21:52 +0200

    create walls geometry and movement

--- src/colorwalls/graph/state/init.ts
+++ src/colorwalls/graph/state/init.ts
@@ -1,30 +1,76 @@
 import { val, stream } from 'tvs-flow/dist/lib/utils/entity-reference'
 import { normalRand } from 'tvs-libs/dist/lib/math/random'
-import { extrudeBottom } from 'tvs-libs/dist/lib/geometry/quad'
+import { extrudeBottom, right, extrudeRight, Quad, divideVertical, divideHorizontal } from 'tvs-libs/dist/lib/geometry/quad'
 import { normal } from 'tvs-libs/dist/lib/geometry/primitives'
 import { tick } from '../events'
-import { mat4 } from 'gl-matrix'
+import { mat4, quat } from 'gl-matrix'
+import { times, flatten } from 'tvs-libs/dist/lib/utils/sequence'
 
 
-export const tileSize = val(10)
+function subdivide (q: Quad) {
+	return flatten(divideVertical(0.5, 0.5, q)
+		.map(q => flatten(divideHorizontal(0.5, 0.5, q))))
+}
 
-export const color = val([normalRand(), normalRand(), normalRand()])
+export const boxSliceCount = val(10)
 
-export const quad = val(extrudeBottom([0, -2, 0], [[-1, 1, 0], [1, 1, 0]]))
+function randomDivide (q: Quad, sliceCount: number) {
+	const quads: Quad[] = []
+	let rest = q
+	for (let i = sliceCount; i > 1; i--) {
+		const upRatio = 1 / i + (Math.random() * 2 - 1) * 0.5 / i
+		const downRatio = 1 / i + (Math.random() * 2 - 1) * 0.5 / i
+		const [left, right] = divideVertical(upRatio, downRatio, rest)
+		quads.push(left)
+		rest = right
+	}
+	quads.push(rest)
+	return quads
+}
 
-export const faceNormal = stream([quad.HOT], q => normal(q))
 
-export const rotation = val(0)
+export const colors = stream(
+	[boxSliceCount.HOT],
+	(count) => times(() => [normalRand(), normalRand(), normalRand()], 4 * count)
+)
+
+export const quad = val(extrudeBottom([0, -9, 0], [[-10, 10, -10], [10, 10, -10]])).reset()
+
+export const box = stream([quad.HOT, boxSliceCount.HOT], (q, count) => {
+	const bk = q
+	const rt = extrudeRight([0, 0, 20], right(bk))
+	const ft = extrudeRight([-20, 0, 0], right(rt))
+	const lf = extrudeRight([0, 0, -20], right(ft))
+	return [
+		randomDivide(bk, count).map(subdivide),
+		randomDivide(rt, count).map(subdivide),
+		randomDivide(ft, count).map(subdivide),
+		randomDivide(lf, count).map(subdivide)
+	]
+})
+
+
+export const faceNormal = stream([box.HOT], b => b.map(q => normal(q[1])))
+
+export const time = val(0)
 .react(
 	[tick.HOT],
-	(_s, tpf) => tpf * 0.0003
+	(self, tpf) => self + tpf
 )
 
-export const transform = stream(
-	[tileSize.HOT],
-	size => mat4.fromScaling(mat4.create(), [size, size, size])
-)
+export const rotation = val(quat.create())
+	.react(
+		[time.HOT],
+		(self, time) => quat.fromEuler(
+			self,
+			Math.sin(0.0007 * time) * 1.1,
+			time * 0.001,
+			Math.sin(0.0008 * time) * 1.1
+		)
+	)
+
+export const transform = val(mat4.create())
 .react(
 	[rotation.HOT],
-	(s, rot) => mat4.rotateY(s, s, rot)
+	(self, rotation) => mat4.fromRotationTranslationScaleOrigin(self, rotation, [0, 0, 0], [1, 1, 1], [0, 100, 0])
 )
--- src/colorwalls/graph/view/geometries.ts
+++ src/colorwalls/graph/view/geometries.ts
@@ -1,15 +1,16 @@
-import { faceNormal, color, quad } from '../state/init'
+import { faceNormal, colors, box, boxSliceCount } from '../state/init'
 import { stream } from 'tvs-flow/dist/lib/utils/entity-reference'
 import { convertStackGLGeometry } from 'tvs-painter/dist/lib/utils/stackgl'
-import { quadTriangles } from 'tvs-libs/dist/lib/geometry/quad'
+import { triangulate } from 'tvs-libs/dist/lib/geometry/quad'
+import { flatten } from 'tvs-libs/dist/lib/utils/sequence'
 
 
 export const plane = stream(
-	[quad.HOT, color.HOT, faceNormal.HOT],
-	(q, c, n) => convertStackGLGeometry({
-		position: q,
-		color: q.map(() => c),
-		normal: q.map(() => n),
-		cells: quadTriangles
+	[box.HOT, colors.HOT, faceNormal.HOT, boxSliceCount.HOT],
+	(b, c, n, sliceCount) => convertStackGLGeometry({
+		position: flatten(flatten(b)),
+		color: flatten(b.map((side, i) => flatten(side.map((slice, j) => slice.map(() => c[i * sliceCount + j]))))),
+		normal: flatten(b.map((side, i) => flatten(side).map(() => n[i]))),
+		cells: triangulate(4 * sliceCount * 4)
 	})
 )
