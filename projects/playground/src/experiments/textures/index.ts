daaed7221d3796f25907569d8073bfe4fb881cb1
Thomas Gorny <thomas.gorny@pm.me>
2020-04-07 02:32:44 +0200

    add thi.ng shader example to textures

--- projects/playground/src/experiments/textures/index.ts
+++ projects/playground/src/experiments/textures/index.ts
@@ -1,8 +1,45 @@
-import { getEffect } from '../../shared-utils/painterState'
-import { painter } from './context'
+import {
+	getEffect,
+	get,
+	dispatch,
+	getFrame,
+	addSystem,
+} from '../../shared-utils/painterState'
+import { painter, events } from './context'
+import { noiseShader } from './shaders'
+import { repeat } from '../../shared-utils/scheduler'
 
 // ===== layers =====
 
-export const noise = getEffect(painter, 'noise')
+export const noise = getEffect(painter, 'noise').update({
+	frag: noiseShader,
+})
 
 export const toNormal = getEffect(painter, 'toNormal')
+
+const noiseFrame = getFrame(painter, 'noise').update({
+	layers: noise,
+})
+
+let time = 0
+
+repeat(tpf => {
+	const d = get('device')
+	d.tpf = tpf
+	// dispatch(events.FRAME)
+
+	time += tpf / 10000
+	noise.update({
+		uniforms: {
+			resolution: [d.canvas.width, d.canvas.height],
+			time,
+		},
+	})
+	painter.compose(noiseFrame).display(noiseFrame)
+}, 'loop')
+
+addSystem('index', (e, s) => {
+	if (e === events.RESIZE) {
+		noiseFrame.update()
+	}
+})
---
+++ projects/playground/src/experiments/textures/shaders.ts
@@ -0,0 +1,65 @@
+import {
+	defn,
+	Vec2Sym,
+	FloatSym,
+	sym,
+	add,
+	vec2,
+	float,
+	ret,
+	vec4,
+	vec3,
+	program,
+	defMain,
+	uniform,
+	TaggedFn3,
+	Vec4Sym,
+	assign,
+	$xy,
+} from '@thi.ng/shader-ast'
+import {
+	additive,
+	aspectCorrectedUV,
+	fit1101,
+	snoise2,
+} from '@thi.ng/shader-ast-stdlib'
+import { targetGLSL, GLSLVersion } from '@thi.ng/shader-ast-glsl'
+
+const fs = targetGLSL({
+	version: GLSLVersion.GLES_100,
+	type: 'fs',
+	prelude: 'precision mediump float;',
+})
+
+let getImage = defn(
+	'vec4',
+	'mainImage',
+	['vec2', 'vec2', 'float'],
+	(fragCoord: Vec2Sym, res: Vec2Sym, time: FloatSym) => {
+		let uv: Vec2Sym
+		let col: FloatSym
+		return [
+			(uv = sym(aspectCorrectedUV(fragCoord, res))),
+			// dynamically create a multi-octave version of `snoise2`
+			// computed over 4 octaves w/ given phase shift and decay
+			// factor (both per octave)
+			(col = sym(
+				additive('vec2', snoise2, 4)(add(uv, time), vec2(2), float(0.5)),
+			)),
+			ret(vec4(vec3(fit1101(col)), 1)),
+		]
+	},
+)
+let resolution: Vec2Sym
+let time: FloatSym
+export const noiseShader = fs(
+	program([
+		(resolution = uniform('vec2', 'resolution')),
+		(time = uniform('float', 'time')),
+		defMain(() => [
+			assign(fs.gl_FragColor, getImage($xy(fs.gl_FragCoord), resolution, time)),
+		]),
+	]),
+)
+
+console.log(fs, noiseShader)
