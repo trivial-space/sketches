f1bf311fe5490e5d52991970a5a0e16a122d9363
Thomas Gorny <thomas.gorny@pm.me>
2020-05-09 22:06:21 +0200

    little tweak

--- projects/playground/src/scratches/index.ts
+++ projects/playground/src/scratches/index.ts
@@ -7,12 +7,12 @@ import { scene } from './renderer'
 state.time = 0
 state.device.sizeMultiplier = window.devicePixelRatio
 
-repeat((tpf) => {
-	state.device.tpf = tpf
-	state.time += tpf / 1000
-	dispatch(events.FRAME)
-	// painter.compose(scene).display(scene)
-}, 'loop')
+// repeat((tpf) => {
+// 	state.device.tpf = tpf
+// 	state.time += tpf / 1000
+// 	dispatch(events.FRAME)
+// 	painter.compose(scene).display(scene)
+// }, 'loop')
 
 addSystem('index', (e) => {
 	if (e === events.RESIZE) {
