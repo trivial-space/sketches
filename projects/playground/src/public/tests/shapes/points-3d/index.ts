b1c69f8aef3b5388743d7d47f8fd1c3bb5faaf0d
Thomas Gorny <thomas.gorny@pm.me>
2021-07-12 22:52:06 +0200

    cleanup points example

--- projects/playground/src/public/tests/shapes/points-3d/index.ts
+++ projects/playground/src/public/tests/shapes/points-3d/index.ts
@@ -1,4 +1,3 @@
-import { repeat } from '../../../../shared-utils/scheduler'
 import { createPoints3DSketch } from '../../../../shared-utils/sketches/points'
 import { times } from 'tvs-libs/dist/utils/sequence'
 import { mat4 } from 'gl-matrix'
@@ -16,11 +15,14 @@ initPerspectiveViewport(Q, {
 const pointCount = 200
 
 const pointsMat = mat4.fromTranslation(mat4.create(), [0, 0, -100])
-const worldMat = mat4.create()
+const viewMat = mat4.create()
 
 const points = createPoints3DSketch(Q, 'points', {
 	pointSize: 5,
 	scalePerspective: true,
+	projectionMat: Q.state.viewPort.camera.projectionMat,
+	viewMat: viewMat,
+
 	positions: times(
 		() => [
 			Math.random() * 100 - 50,
@@ -29,10 +31,12 @@ const points = createPoints3DSketch(Q, 'points', {
 		],
 		pointCount,
 	),
+
 	colors: times(
 		() => [Math.random(), Math.random(), Math.random(), 1],
 		pointCount,
 	),
+
 	drawSettings: {
 		enable: [Q.gl.DEPTH_TEST],
 		clearColor: [1, 0, 1, 1],
@@ -41,16 +45,12 @@ const points = createPoints3DSketch(Q, 'points', {
 })
 
 addToLoop((tpf) => {
-	mat4.rotateY(pointsMat, pointsMat, 0.01)
-	const view = mat4.mul(worldMat, Q.state.viewPort.camera.viewMat, pointsMat)
-
-	points.update({
-		projectionMat: Q.state.viewPort.camera.projectionMat,
-		viewMat: view,
-	})
-
+	// make camera work
 	Q.state.device.tpf = tpf
 	Q.emit(baseEvents.FRAME)
+
+	mat4.rotateY(pointsMat, pointsMat, 0.01)
+	mat4.mul(viewMat, Q.state.viewPort.camera.viewMat, pointsMat)
 
 	Q.painter.draw(points.sketch)
 }, 'loop')
