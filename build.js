7c1a4f0adbaf9da5953818b8330a3fad1cab5d6d
Thomas Gorny <thomas.gorny@pm.me>
2023-08-17 15:31:14 +0200

    generate sketch index page

--- build.js
+++ build.js
@@ -2,6 +2,8 @@ import { build } from 'vite'
 import * as glob from 'glob'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { readFile, writeFile } from 'node:fs/promises'
+import * as cheerio from 'cheerio'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,29 +15,46 @@ async function makeBuild() {
 		cwd: publicSrcDir,
 	})
 
-	await build({
-		root: publicSrcDir,
-		plugins: [],
-		build: {
-			assetsDir: '__assets',
-			rollupOptions: {
-				input: apps.reduce((entries, entry, i) => {
-					entries[i] = resolve(publicSrcDir, entry)
-					return entries
-				}, {}),
-			},
-			outDir: resolve(__dirname, 'public'),
-		},
-	})
+	const indexHtml = await readFile(resolve(publicSrcDir, 'index.html'), 'utf-8')
+	console.log(apps, indexHtml)
+	const $ = cheerio.load(indexHtml)
+	$('ul').html(
+		apps
+			.filter((a) => a !== 'index.html')
+			.map((a) => a.replace(/\/index\.html$/, ''))
+			.sort((a, b) => a.localeCompare(b))
+			.map((a) => `<li><a href="/${a}/">${a}</a></li>`)
+			.join('\n'),
+	)
+	await writeFile(resolve(publicSrcDir, 'index.html'), $.html())
 
-	for (const app of apps) {
-		const appDir = dirname(app)
-
+	if (!process.argv.includes('--skip-main')) {
 		await build({
-			root: resolve(publicSrcDir, appDir),
+			root: publicSrcDir,
 			plugins: [],
-			build: { outDir: resolve(__dirname, 'dist', appDir) },
+			build: {
+				assetsDir: '__assets',
+				rollupOptions: {
+					input: apps.reduce((entries, entry, i) => {
+						entries[i] = resolve(publicSrcDir, entry)
+						return entries
+					}, {}),
+				},
+				outDir: resolve(__dirname, 'public'),
+			},
 		})
+	}
+
+	if (process.argv.includes('-a') || process.argv.includes('--all')) {
+		for (const app of apps) {
+			const appDir = dirname(app)
+
+			await build({
+				root: resolve(publicSrcDir, appDir),
+				plugins: [],
+				build: { outDir: resolve(__dirname, 'dist', appDir) },
+			})
+		}
 	}
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
-import { directoryPlugin } from 'vite-plugin-list-directory-contents'
-import * as path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const config = defineConfig(async () => {
 	return {
 		root: 'src/public',
-		plugins: [
-			directoryPlugin({ baseDir: path.resolve(__dirname, 'src/public') }),
-		],
+		plugins: [],
 		server: {
 			port: 8000,
 		},
