64887f83d57d283980a38e7d30f2946500ee26a4
Thomas Gorny <thomas.gorny@pm.me>
2024-01-05 18:26:46 +0100

    tweak

--- src/public/experiments/squeegee-wasm/crate/src/lib.rs
+++ src/public/experiments/squeegee-wasm/crate/src/lib.rs
@@ -17,9 +17,11 @@ struct State {
     height: f32,
     time_since_gravity_update: f32,
     time_to_next_gravity_update: f32,
+    switch_sides: bool,
+    is_top: bool,
 }
 
-const GRAVITY_JUMP_DURATION: f32 = 3.5;
+const GRAVITY_JUMP_DURATION: f32 = 3.0;
 const SPRING_LENGTH: f32 = 200.;
 
 #[wasm_bindgen]
@@ -51,9 +53,16 @@ pub fn update(seconds_per_frame: f32) -> JsValue {
             s.time_to_next_gravity_update =
                 GRAVITY_JUMP_DURATION + random_normal() * GRAVITY_JUMP_DURATION * 0.4;
 
+            let is_top = s.gravity_center.y == gravity_height;
+            let same_side = is_top == s.is_top;
+            let switch_sides = s.switch_sides && same_side;
+
+            s.switch_sides = !same_side;
+            s.is_top = is_top;
+
             s.gravity_center = vec2(
                 rand_f32() * s.width / 2. + s.width / 4.,
-                if s.gravity_center.y == gravity_height {
+                if switch_sides {
                     s.height - gravity_height
                 } else {
                     gravity_height
@@ -63,15 +72,13 @@ pub fn update(seconds_per_frame: f32) -> JsValue {
             s.time_since_gravity_update = gravity_time;
         }
 
-        let is_top = s.gravity_center.y == gravity_height;
-
         // attract puller to gravity_center
 
         let puller_to_gravity = s.gravity_center - s.puller.pos;
-        let puller_to_gravity_force = puller_to_gravity * seconds_per_frame * 0.14;
+        let puller_to_gravity_force = puller_to_gravity * seconds_per_frame * 0.01;
 
-        s.puller.vel *= 0.998;
-        s.puller.vel += puller_to_gravity_force.clamp(vec2(-0.08, -0.08), vec2(0.08, 0.08));
+        s.puller.vel *= 0.99;
+        s.puller.vel += puller_to_gravity_force; //.clamp(vec2(-0.08, -0.08), vec2(0.08, 0.08));
         s.puller.pos += s.puller.vel;
 
         // attract brush to puller
@@ -83,12 +90,8 @@ pub fn update(seconds_per_frame: f32) -> JsValue {
             brush_to_puller_dir * (brush_to_puller_dist - SPRING_LENGTH) * 1.9;
 
         s.brush.vel *= 0.985;
+        s.brush.vel += s.brush.vel * 0.0001;
         s.brush.vel += brush_to_puller_force;
-        // s.brush.vel += if is_top {
-        //     vec2(0., 3. * seconds_per_frame)
-        // } else {
-        //     vec2(0., -3. * seconds_per_frame)
-        // };
         s.brush.pos += s.brush.vel * seconds_per_frame;
     });
 
