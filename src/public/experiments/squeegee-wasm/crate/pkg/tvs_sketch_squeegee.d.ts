50252427055791527de33b0e64425b65e90c94c0
Thomas Gorny <thomas.gorny@pm.me>
2024-01-06 13:20:45 +0100

    git add wasm build

---
+++ src/public/experiments/squeegee-wasm/crate/pkg/tvs_sketch_squeegee.d.ts
@@ -0,0 +1,44 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} width
+* @param {number} height
+*/
+export function setup(width: number, height: number): void;
+/**
+* @param {number} seconds_per_frame
+* @returns {any}
+*/
+export function update(seconds_per_frame: number): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly setup: (a: number, b: number) => void;
+  readonly update: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ src/public/experiments/squeegee-wasm/crate/pkg/tvs_sketch_squeegee_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function setup(a: number, b: number): void;
+export function update(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;
