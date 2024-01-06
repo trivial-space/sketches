7cd6f94c4bde8ce4ff1b8572c4b0bb53a992b803
Thomas Gorny <thomas.gorny@pm.me>
2024-01-06 14:26:48 +0100

    tweak physics

--- src/public/experiments/squeegee-wasm/crate/src/lib.rs
+++ src/public/experiments/squeegee-wasm/crate/src/lib.rs
@@ -19,19 +19,22 @@ struct State {
     time_to_next_gravity_update: f32,
     switch_sides: bool,
     is_top: bool,
+    spring_length: f32,
+    jump_duration: f32,
 }
-
-const GRAVITY_JUMP_DURATION: f32 = 3.0;
-const SPRING_LENGTH: f32 = 200.;
 
 #[wasm_bindgen]
 pub fn setup(width: f32, height: f32) {
+    let spring_length = width / 6.;
+
     State::mutate(|s| {
         s.width = width;
         s.height = height;
         s.gravity_center = vec2(rand_f32() * width, if random() { height } else { 0. });
         s.puller.pos = vec2(width / 2., height / 2.);
-        s.brush.pos = vec2(width / 2. + SPRING_LENGTH, height / 2.);
+        s.brush.pos = vec2(width / 2. + spring_length, height / 2.);
+        s.spring_length = spring_length;
+        s.jump_duration = height / 1200.;
     });
 }
 
@@ -45,24 +48,25 @@ struct FrameData {
 #[wasm_bindgen]
 pub fn update(seconds_per_frame: f32) -> JsValue {
     State::mutate(|s| {
-        let gravity_time = s.time_since_gravity_update + seconds_per_frame;
-        let gravity_height = s.height / 6.;
+        let time = seconds_per_frame.min(0.05);
+        let gravity_time = s.time_since_gravity_update + time;
+        let gravity_height = s.height / 4.;
 
         if gravity_time > s.time_to_next_gravity_update {
             s.time_since_gravity_update = 0.0;
             s.time_to_next_gravity_update =
-                GRAVITY_JUMP_DURATION + random_normal() * GRAVITY_JUMP_DURATION * 0.4;
+                s.jump_duration + random_normal() * s.jump_duration * 0.4;
 
             let is_top = s.gravity_center.y == gravity_height;
-            let same_side = is_top == s.is_top;
-            let switch_sides = s.switch_sides && same_side;
 
-            s.switch_sides = !same_side;
-            s.is_top = is_top;
+            s.switch_sides = !s.switch_sides;
+            if s.switch_sides {
+                s.is_top = is_top;
+            }
 
             s.gravity_center = vec2(
-                rand_f32() * s.width / 2. + s.width / 4.,
-                if switch_sides {
+                rand_f32() * s.width / 1.5 + s.width / 6.,
+                if s.is_top {
                     s.height - gravity_height
                 } else {
                     gravity_height
@@ -75,7 +79,7 @@ pub fn update(seconds_per_frame: f32) -> JsValue {
         // attract puller to gravity_center
 
         let puller_to_gravity = s.gravity_center - s.puller.pos;
-        let puller_to_gravity_force = puller_to_gravity * seconds_per_frame * 0.01;
+        let puller_to_gravity_force = puller_to_gravity * time * s.height / 250000.;
 
         s.puller.vel *= 0.99;
         s.puller.vel += puller_to_gravity_force; //.clamp(vec2(-0.08, -0.08), vec2(0.08, 0.08));
@@ -87,12 +91,12 @@ pub fn update(seconds_per_frame: f32) -> JsValue {
         let brush_to_puller_dist = brush_to_puller.length();
         let brush_to_puller_dir = brush_to_puller / brush_to_puller_dist;
         let brush_to_puller_force =
-            brush_to_puller_dir * (brush_to_puller_dist - SPRING_LENGTH) * 1.9;
+            brush_to_puller_dir * (brush_to_puller_dist - s.spring_length) * 0.3;
 
-        s.brush.vel *= 0.985;
+        s.brush.vel *= 0.995;
         s.brush.vel += s.brush.vel * 0.0001;
         s.brush.vel += brush_to_puller_force;
-        s.brush.pos += s.brush.vel * seconds_per_frame;
+        s.brush.pos += s.brush.vel * time;
     });
 
     let s = State::read();
