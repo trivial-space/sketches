345a61893f449d7d1f0d33acc04d2649af86b806
Thomas Gorny <thomas.gorny@pm.me>
2024-09-19 23:06:22 +0200

    indicate video loading

--- src/public/works/homage/index.ts
+++ src/public/works/homage/index.ts
@@ -13,30 +13,38 @@ import { videos } from './state/videos'
 const d = Q.get('device')
 // d.sizeMultiplier = 1.5
 
-videos.then((vs) => {
-	function startVideos() {
-		vs.forEach((v) => {
-			if (!(v.currentTime > 0 && !v.paused && !v.ended && v.readyState > 2)) {
-				v.play()
-			}
-			v.muted = false
-		})
-		d.canvas.removeEventListener('mousedown', startVideos)
-		d.canvas.removeEventListener('touchstart', startVideos)
-	}
-	d.canvas.addEventListener('mousedown', startVideos)
-	d.canvas.addEventListener('touchstart', startVideos)
+const loadingMessage = document.getElementById('loading-message')
+videos
+	.then((vs) => {
+		loadingMessage!.remove()
+		function startVideos() {
+			vs.forEach((v) => {
+				if (!(v.currentTime > 0 && !v.paused && !v.ended && v.readyState > 2)) {
+					v.play()
+				}
+				v.muted = false
+			})
+			d.canvas.removeEventListener('mousedown', startVideos)
+			d.canvas.removeEventListener('touchstart', startVideos)
+		}
+		d.canvas.addEventListener('mousedown', startVideos)
+		d.canvas.addEventListener('touchstart', startVideos)
 
-	addToLoop(() => {
-		Q.emit(events.FRAME)
-		videoTextures.forEach((t, i) =>
-			t.update({ texture: { ...videoTextureData, asset: vs[i] } }),
-		)
-		Q.painter.compose(...videoLights, mirrorScene, scene)
-	}, 'render')
+		addToLoop(() => {
+			Q.emit(events.FRAME)
+			videoTextures.forEach((t, i) =>
+				t.update({ texture: { ...videoTextureData, asset: vs[i] } }),
+			)
+			Q.painter.compose(...videoLights, mirrorScene, scene)
+		}, 'render')
 
-	startLoop()
-})
+		startLoop()
+	})
+	.catch((e) => {
+		loadingMessage!.textContent =
+			'Something went wrong, failed to load videos :('
+		console.error('Failed to load videos', e)
+	})
 
 if (import.meta.hot) {
 	import.meta.hot.accept()
