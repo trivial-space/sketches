f0683c926e5d020c9db6af49744a2cbe8b018d72
Thomas Gorny <thomas.gorny@pm.me>
2024-09-20 12:12:27 +0200

    add proxy cloudflare functions

---
+++ functions/assets/[[args]].ts
@@ -0,0 +1,11 @@
+interface Env {
+	REMOTE_ASSETS_URL: string
+}
+
+export const onRequest: PagesFunction<Env> = async (c) => {
+	const newUrl = c.env.REMOTE_ASSETS_URL
+	const r = c.request as Request
+	const url = r.url.replace(/.*api\//, newUrl + '/')
+
+	return fetch(url, r)
+}
